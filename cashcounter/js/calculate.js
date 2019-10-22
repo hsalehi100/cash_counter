2673d8275b767cb707c3c968b8e4495b62b71572
Hani Salehi <hsalehi313@gmail.com>
2019-10-22 16:26:24 -0400

    Cash model created
    need to fix javascript issue for total

---
+++ cashcounter/js/calculate.js
@@ -0,0 +1,34 @@
+sumar = () => {
+    const five_cent = document.getElementById("five_cent").value * 0.05;
+    const ten_cent = document.getElementById("ten_cent").value * 0.10;
+    const twentyfive_cent = document.getElementById("twentyfive_cent").value * 0.25;
+    const loonnie = document.getElementById("loonie").value * 1;
+    const toonie = document.getElementById("toonie").value * 2;
+    const five = document.getElementById("five").value * 5;
+    const ten = document.getElementById("ten").value * 10;
+    const twenty = document.getElementById("twenty").value * 20;
+    const fifty = document.getElementById("fifty").value * 50;
+    const hundred = document.getElementById("hundred").value * 100;
+    const five_roll = document.getElementById("five_roll").value * 2;
+    const ten_roll = document.getElementById("ten_roll").value * 5;
+    const twentyfive_roll = document.getElementById("twentyfive_roll").value * 10;
+    const loonie_roll = document.getElementById("loonie_roll").value * 25;
+    const toonie_roll = document.getElementById("toonie_roll").value * 50;
+
+    const coins = five_cent + ten_cent + twentyfive_cent + loonie + toonie;
+    const bills = five + ten + twenty + fifty + hundred;
+    const rolls = five_roll + ten_roll + twentyfive_roll + loonie_roll + toonie_roll;
+
+    const total = coins + bills + rolls;
+    document.getElementById("total").innerHTML = "TOTAL: $" + total.toString();
+}
+
+
+reset = () => {
+    document.getElementById("to500").value = 0
+    document.getElementById("to200").value = 0
+    document.getElementById("to100").value = 0
+    document.getElementById("to50").value = 0
+    document.getElementById("to20").value = 0
+    document.getElementById("total").innerHTML = "0";
+}
