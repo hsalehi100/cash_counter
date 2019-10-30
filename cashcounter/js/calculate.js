0199051464f13b2e4f0c5e074fcfc634b0c39234
Hani Salehi <hsalehi313@gmail.com>
2019-10-30 13:24:33 -0400

    Working Version

--- cashcounter/js/calculate.js
+++ cashcounter/js/calculate.js
@@ -1,34 +1,76 @@
-sumar = () => {
-    const five_cent = document.getElementById("five_cent").value * 0.05;
-    const ten_cent = document.getElementById("ten_cent").value * 0.10;
-    const twentyfive_cent = document.getElementById("twentyfive_cent").value * 0.25;
-    const loonnie = document.getElementById("loonie").value * 1;
-    const toonie = document.getElementById("toonie").value * 2;
-    const five = document.getElementById("five").value * 5;
-    const ten = document.getElementById("ten").value * 10;
-    const twenty = document.getElementById("twenty").value * 20;
-    const fifty = document.getElementById("fifty").value * 50;
-    const hundred = document.getElementById("hundred").value * 100;
-    const five_roll = document.getElementById("five_roll").value * 2;
-    const ten_roll = document.getElementById("ten_roll").value * 5;
-    const twentyfive_roll = document.getElementById("twentyfive_roll").value * 10;
-    const loonie_roll = document.getElementById("loonie_roll").value * 25;
-    const toonie_roll = document.getElementById("toonie_roll").value * 50;
+function calculate() {
+  //coins
+  let fcent = document.getElementsByName("fcent")[0].value * 0.05;
+  let tcent = document.getElementsByName("tcent")[0].value * 0.10;
+  let tfcent = document.getElementsByName("tfcent")[0].value * 0.25;
+  let loonie = document.getElementsByName("loonie")[0].value * 1;
+  let toonie = document.getElementsByName("toonie")[0].value * 2;
 
-    const coins = five_cent + ten_cent + twentyfive_cent + loonie + toonie;
-    const bills = five + ten + twenty + fifty + hundred;
-    const rolls = five_roll + ten_roll + twentyfive_roll + loonie_roll + toonie_roll;
+  //bills
+  let five = document.getElementsByName("five")[0].value * 5;
+  let ten = document.getElementsByName("ten")[0].value * 10;
+  let twenty = document.getElementsByName("twenty")[0].value * 20;
+  let fifty = document.getElementsByName("fifty")[0].value * 50;
+  let hundred = document.getElementsByName("hundred")[0].value * 100;
 
-    const total = coins + bills + rolls;
-    document.getElementById("total").innerHTML = "TOTAL: $" + total.toString();
+  //coins rolls
+  let froll = document.getElementsByName("froll")[0].value * 2;
+  let troll = document.getElementsByName("troll")[0].value * 5;
+  let tfroll = document.getElementsByName("tfroll")[0].value * 10;
+  let lroll = document.getElementsByName("lroll")[0].value * 25;
+  let tooroll = document.getElementsByName("tooroll")[0].value * 50;
+
+  let totalCoins = Number(fcent) + Number(tcent) + Number(tfcent) + Number(loonie) + Number(toonie);
+  let totalBills = Number(five) + Number(ten) + Number(twenty) + Number(fifty) + Number(hundred);
+  let totalRolls = Number(froll) + Number(troll) + Number(tfroll) + Number(lroll) + Number(tooroll);
+
+  let total = totalCoins + totalBills + totalRolls;
+  document.getElementsByName("total")[0].value = total;
+}
+
+function tills() {
+  let till1 = document.getElementsByName("till1")[0].value;
+  let till2 = document.getElementsByName("till2")[0].value;
+  let till3 = document.getElementsByName("till3")[0].value;
+
+  let total = Number(till1) + Number(till2) + Number(till3);
+  let deposit = (total - 500).toFixed(2);
+
+
+  document.getElementsByName("tot")[0].value = total;
+  document.getElementsByName("deposit")[0].value = deposit;
+}
+
+function reset_tills() {
+  document.getElementsByName("till1")[0].value = 0;
+  document.getElementsByName("till2")[0].value = 0;
+  document.getElementsByName("till3")[0].value = 0;
+
+  document.getElementsByName("tot")[0].value = 0;
+  document.getElementsByName("deposit")[0].value = 0;
 }
 
 
 reset = () => {
-    document.getElementById("to500").value = 0
-    document.getElementById("to200").value = 0
-    document.getElementById("to100").value = 0
-    document.getElementById("to50").value = 0
-    document.getElementById("to20").value = 0
-    document.getElementById("total").innerHTML = "0";
+  document.getElementsByName("fcent")[0].value = 0;
+  document.getElementsByName("tcent")[0].value = 0;
+  document.getElementsByName("tfcent")[0].value = 0;
+  document.getElementsByName("loonie")[0].value = 0;
+  document.getElementsByName("toonie")[0].value = 0;
+
+  //bills
+  document.getElementsByName("five")[0].value = 0;
+  document.getElementsByName("ten")[0].value = 0;
+  document.getElementsByName("twenty")[0].value = 0;
+  document.getElementsByName("fifty")[0].value = 0;
+  document.getElementsByName("hundred")[0].value = 0;
+
+  //coins rolls
+  document.getElementsByName("froll")[0].value = 0;
+  document.getElementsByName("troll")[0].value = 0;
+  document.getElementsByName("tfroll")[0].value = 0;
+  document.getElementsByName("lroll")[0].value = 0;
+  document.getElementsByName("tooroll")[0].value = 0;
+
+  document.getElementsByName("total")[0].value = 0;
 }
